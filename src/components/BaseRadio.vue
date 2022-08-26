afbf8c7a9adc85e91f841c7e7b03910cb5b07199
Micha Kraußer <michak@synphonyte.com>
2022-08-26 02:50:31 -0600

    added CheckBox and Radio components

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "BaseRadio"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "BaseSelect"
+}
+</script>
+
+<style scoped>
+
+</style>
